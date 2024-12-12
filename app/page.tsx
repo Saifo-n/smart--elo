0fd37b6c438b553c6f7bb1fc56f5460410964825
Saifo-n <fonfonfon.s2556@gmail.com>
2024-12-12 20:28:08 +0700

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-import Image  from "next/image";
-import { createMetadataComponents } from "next/dist/lib/metadata/metadata";
-import { loadComponents } from "next/dist/server/load-components";
-import Header from "@/components/header";
+// app/page.tsx
+import Home from "@components/home";  // ใช้ alias @components
 
-export default function Home(){
-    return(
-        <main>
-            <h1> Hello </h1>
-        </main>
-    )
+export default function Page() {
+  return <Home />;  // แสดงคอมโพเนนต์ Home ในหน้าแรก
 }
